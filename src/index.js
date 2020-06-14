98bacba6690e00e3eb6cb07cf1b975e5f7845fd2
yamachan0625 <napoli.080625@gmail.com>
2020-06-14 20:06:08 +0900

    implement getting startec after webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+
 function component() {
   const element = document.createElement('div');
   const array = ['Hello', 'webpack'];
-  element.innerHTML = _.join(array, '');
+  element.innerHTML = _.join(array, ' ');
   return element;
 }
 
