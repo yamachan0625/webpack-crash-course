5bc2feac9ebc38a560cea283c41c924996b075a6
yamachan0625 <napoli.080625@gmail.com>
2020-06-16 00:56:09 +0900

    implement uglifyjs-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import './style.scss';
 
+console.log('hello');
+
 ReactDOM.render(<div>Hello, react</div>, document.getElementById('root'));
