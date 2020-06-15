35860b2c48781aa8946efcc4006da640fefa2846
yamachan0625 <napoli.080625@gmail.com>
2020-06-16 01:11:32 +0900

    implement optimaize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist');
 module.exports = {
@@ -50,6 +51,7 @@ module.exports = {
           },
         },
       }),
+      new OptimizeCSSAssetsPlugin({}),
     ],
   },
 };
